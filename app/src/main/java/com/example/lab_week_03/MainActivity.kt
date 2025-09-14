3fd0885131d082171fe7e216e461f5acea7bf547
Mochamad Daffa <mochamaddaffa05@gmail.com>
2025-09-14 20:07:18 +0700

    Commit No.4 – use jetpack navigation

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -5,39 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        if (savedInstanceState == null) {
-            findViewById<FragmentContainerView>(R.id.fragment_container).let { containerLayout ->
-                val listFragment = ListFragment()
-                supportFragmentManager.beginTransaction()
-                    .add(containerLayout.id, listFragment)
-                    .commit()
-            }
-        }
-    }
-
-    // This replaces CoffeeListener — we call this directly from ListFragment
-    fun onSelected(id: Int) {
-        findViewById<FragmentContainerView>(R.id.fragment_container).let { containerLayout ->
-            val detailFragment = DetailFragment.newInstance(id)
-            supportFragmentManager.beginTransaction()
-                .replace(containerLayout.id, detailFragment)
-                .addToBackStack(null)
-                .commit()
         }
     }
 
